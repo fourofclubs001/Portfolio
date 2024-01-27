d408c93d706238e2f12c05028a96c6be8cc11067
fourofclubs001 <lucasvitali001@gmail.com>
2024-01-27 18:31:29 -0300

    header as macro for name parametrization

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import json
+import os
+
+path = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__, template_folder = "templates")
 
 info = {}
-info_path = "C:/Users/usuario/repositories/Portfolio/info.json"
+info_path = os.path.join(path, "info.json")
 
 with open(info_path, 'r') as info_file:
     info = json.load(info_file)
