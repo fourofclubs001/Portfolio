a3f041d254a7e01bdcff236858625889f99ebc69
fourofclubs001 <lucasvitali001@gmail.com>
2024-02-01 18:39:29 -0300

    ready for re do the contact form with out php-email-class

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for
+from modules.ContactForm import ContactForm
 import json
 import os
 
 path = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__, template_folder = "templates")
+app.config['SECRET_KEY'] = 'secretkey'
 
 info = {}
 info_path = os.path.join(path, "info.json")
@@ -12,10 +14,27 @@ info_path = os.path.join(path, "info.json")
 with open(info_path, 'r') as info_file:
     info = json.load(info_file)
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def index():
-    return render_template("index.html", info = info)
 
+    contactForm = ContactForm()
+    info["contact"]["form"] = contactForm
+
+    if contactForm.validate_on_submit():
+
+        name = contactForm.name
+        email = contactForm.email
+        subject = contactForm.subject
+        message = contactForm.message
+
+        print(name)
+        print(email)
+        print(subject)
+        print(message)
+
+        return render_template("index.html", info = info)
+
+    return render_template("index.html", info = info)
 
 if __name__ == "__main__":
 
---
+++ modules/ContactForm.py
@@ -0,0 +1,38 @@
+from flask_wtf import FlaskForm 
+from wtforms import StringField, EmailField, TextAreaField
+from wtforms.validators import InputRequired 
+from werkzeug.security import generate_password_hash 
+
+class ContactForm(FlaskForm):
+
+	name_atributtes = {
+		"type":"text",
+		"class":"form-control", 
+		"id":"name"
+	}
+
+	email_atributtes = {
+		"type":"email", 
+		"class":"form-control",
+		"id":"email"
+	}
+
+	subject_atributtes = {
+		"type":"text",
+		"class":"form-control",
+		"id":"subject"
+	}
+
+	message_atributtes = {
+		"class":"form-control",
+		"rows":"10"
+	}
+
+	name = StringField('Name', validators=[InputRequired()],
+						render_kw = name_atributtes)
+	email = EmailField('Email', validators=[InputRequired()],
+						render_kw = email_atributtes)
+	subject = StringField('Subject', validators=[InputRequired()],
+					       render_kw = subject_atributtes)
+	message = TextAreaField('Message', validators=[InputRequired()],
+						 	render_kw = message_atributtes)
