f6955a3333479663308ab56a6c6b05dd69a52fd4
fourofclubs001 <lucasvitali001@gmail.com>
2024-02-10 17:51:43 -0300

    form send mail via mailgun test

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from modules.ContactForm import ContactForm
+import requests
 import secrets
 import json
 import os
@@ -23,12 +24,24 @@ def index():
 
     if contactForm.validate_on_submit():
 
-        name = contactForm.name
-        email = contactForm.email
-        subject = contactForm.subject
-        message = contactForm.message
+        name = contactForm.name.data
+        email = contactForm.email.data
+        subject = contactForm.subject.data
+        message = contactForm.message.data
 
-        return render_template("contact_form_response.html")
+        body = 'Name: ' + name + ' Email: ' + email + '\n' + message
+
+        response = requests.post(
+		"https://api.mailgun.net/v3/sandbox4f5311015adf4863ba3ea4f0ade788be.mailgun.org/messages",
+		auth=("api", "dc786b5bb50ca2f1402996747169b295-8c90f339-6f386df1"),
+		data={"from": "postmaster@sandbox4f5311015adf4863ba3ea4f0ade788be.mailgun.org",
+			"to": ["lucasvitali001@gmail.com"],
+			"subject": subject,
+			"text": body})
+
+        isEmailSent = False
+
+        return render_template("contact_form_response.html", isEmailSent = isEmailSent)
 
     return render_template("index.html", info = info)
 
