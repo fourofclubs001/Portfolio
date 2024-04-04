e81d05f5122c050b01166a61e755090ae08f1335
fourofclubs001 <lucasvitali001@gmail.com>
2024-04-04 20:36:28 -0300

    no form

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ path = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__, template_folder = "templates")
 app.config['SECRET_KEY'] = secrets.token_hex(32)
 
-email_api_key = os.environ.get('MAILGUN_API_KEY')
-
 info = {}
 info_path = os.path.join(path, "info.json")
 
@@ -20,30 +18,6 @@ with open(info_path, 'r') as info_file:
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
-
-    contactForm = ContactForm()
-    info["contact"]["form"] = contactForm
-
-    if contactForm.validate_on_submit():
-
-        name = contactForm.name.data
-        email = contactForm.email.data
-        subject = contactForm.subject.data
-        message = contactForm.message.data
-
-        body = 'Name: ' + name + ' Email: ' + email + '\n' + message
-
-        response = requests.post(
-		"https://api.mailgun.net/v3/sandbox4f5311015adf4863ba3ea4f0ade788be.mailgun.org/messages",
-		auth=("api", email_api_key),
-		data={"from": "postmaster@sandbox4f5311015adf4863ba3ea4f0ade788be.mailgun.org",
-			"to": ["lucasvitali001@gmail.com"],
-			"subject": subject,
-			"text": body})
-
-        isEmailSent = False
-
-        return render_template("contact_form_response.html", isEmailSent = isEmailSent)
 
     return render_template("index.html", info = info)
 
